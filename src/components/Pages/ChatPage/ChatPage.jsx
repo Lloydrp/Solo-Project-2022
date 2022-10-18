08695575d3e47df7cebc18e0f49925133aa9bd24
Lloyd Prock <lloyd.prock@gmail.com>
2022-10-17 23:16:40 -0500

    added skeleton pages

---
+++ src/components/Pages/ChatPage/ChatPage.jsx
@@ -0,0 +1,9 @@
+function ChatPage() {
+  return (
+    <main>
+      <h2>Chat Page</h2>
+    </main>
+  );
+}
+
+export default ChatPage;
---
+++ src/components/Pages/SchedulePage/SchedulePage.jsx
@@ -0,0 +1,9 @@
+function SchedulePage() {
+  return (
+    <main>
+      <h2>Schedule Page</h2>
+    </main>
+  );
+}
+
+export default SchedulePage;

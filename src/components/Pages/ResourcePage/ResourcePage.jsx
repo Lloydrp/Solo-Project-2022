bb11f5f65fde92672de0ba826609e5f7a6357fe6
Lloyd Prock <lloyd.prock@gmail.com>
2022-10-24 14:39:08 -0500

    swiching to file and image seperately

--- src/components/Pages/ResourcePage/ResourcePage.jsx
+++ src/components/Pages/ResourcePage/ResourcePage.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { PickerOverlay } from "filestack-react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
@@ -14,16 +15,28 @@ function ResourcePage() {
   );
   const history = useHistory();
   const dispatch = useDispatch();
-  // File_types are 0 for file, 1 for links, and 2 for images. 3 is default for all files
-  const [currentResource, setCurrentResource] = useState(3);
+  // File_types are 0 for file links, 1 for images. 2 is default for all files
+  const [currentResource, setCurrentResource] = useState(2);
   const [toggleModal, setToggleModal] = useState(false);
   const [searchText, setSearchText] = useState("");
+  const [uploadFile, setUploadFile] = useState({
+    file_url: null,
+    file_type: "image",
+    description: "",
+  });
 
   const resourceArray = organization.orgResources.filter(
     (item) =>
-      (currentResource === 3 && item !== null) ||
+      (currentResource === 2 && item !== null) ||
       Number(item?.file_type) === Number(currentResource)
   );
+
+  const basicOptions = {
+    accept: ["image/*", "audio/*"], // support both image and audio uploads
+    fromSources: ["local_file_system"],
+    maxSize: 1024 * 1024,
+    maxFiles: 1,
+  };
 
   function handleAddResource() {
     setToggleModal(true);
@@ -59,6 +72,30 @@ function ResourcePage() {
     dispatch({ type: "FETCH_ORGANIZATION", payload: { id: params.orgid } });
   }, []);
 
+  function onSubmit(event) {
+    event.preventDefault();
+    // this.props.dispatch({
+    //    type: 'SEND_UPLOAD',
+    //    payload: this.state // { file_type, file_url, description }
+    // });
+    setUploadFile({
+      file_url: null,
+      file_type: "image",
+      description: "",
+    });
+  }
+
+  function onSuccess(result) {
+    console.log("Result from filestack success: ", result);
+    setUploadFile({
+      file_url: result.filesUploaded[0].url,
+    });
+  }
+
+  function onError(error) {
+    console.error("error", error);
+  }
+
   return (
     <main>
       {toggleModal && (
@@ -72,7 +109,7 @@ function ResourcePage() {
       <section className="org-container">
         <nav>
           {userOrganization.is_admin && (
-            <button onClick={handleAddResource}>Add Resource</button>
+            <button onClick={handleAddResource}>Add File/Link</button>
           )}
 
           <br />
@@ -97,13 +134,51 @@ function ResourcePage() {
         <div className="org-sub-container">
           <nav>
             <ul className="org-nav">
-              <li onClick={() => setCurrentResource(3)}>All</li>
-              <li onClick={() => setCurrentResource(0)}>Files</li>
-              <li onClick={() => setCurrentResource(1)}>Links</li>
-              <li onClick={() => setCurrentResource(2)}>Images</li>
+              <li onClick={() => setCurrentResource(2)}>All</li>
+              <li onClick={() => setCurrentResource(0)}>File Links</li>
+              <li onClick={() => setCurrentResource(1)}>Images</li>
             </ul>
           </nav>
           <div>File Preview Section</div>
+          {/* <>
+            <form onSubmit={onSubmit}>
+              <h2>Upload New File</h2>
+              File to upload:
+              <PickerOverlay
+                apikey={process.env.REACT_APP_FILESTACK_API_KEY}
+                buttonText="Upload Image"
+                buttonClass="ui medium button gray"
+                options={basicOptions}
+                onSuccess={onSuccess}
+                onError={onError}
+              />
+              <br />
+              File Type:
+              <select
+                onChange={(e) =>
+                  setUploadFile({ ...uploadFile, file_type: e.target.value })
+                }
+                value={uploadFile.file_type}
+              >
+                <option value="image">Image</option>
+                <option value="audio">Audio</option>
+              </select>
+              {uploadFile.file_url && (
+                <p>Uploaded Image URL: {uploadFile.file_url}</p>
+              )}
+              <br />
+              Description:{" "}
+              <input
+                onChange={(e) =>
+                  setUploadFile({ ...uploadFile, description: e.target.value })
+                }
+                value={uploadFile.description}
+              />
+              <div>
+                <button type="submit">Submit Image</button>
+              </div>
+            </form>
+          </> */}
         </div>
       </section>
     </main>

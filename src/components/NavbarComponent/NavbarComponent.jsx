f96cc6f2eedc77641ff8fe7b571f636a1cbd3e47
Lloyd Prock <lloyd.prock@gmail.com>
2022-10-26 16:54:22 -0500

    updated navbar

--- src/components/NavbarComponent/NavbarComponent.jsx
+++ src/components/NavbarComponent/NavbarComponent.jsx
@@ -9,23 +9,31 @@ function NavbarComponent({ orgid }) {
 
   return (
     <>
-      <Navbar bg="primary" variant="dark">
-        <Container>
-          <Navbar.Brand href="#/home">Organization Station</Navbar.Brand>
-          {!user.id && (
+      <Navbar collapseOnSelect expand="sm" bg="primary" variant="dark">
+        <Container fluid>
+          <Navbar.Brand className="fs-2" href="#/home">
+            Organization Station
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#/login">Login/Register</Nav.Link>
+              <Nav.Link className="mx-auto" href={`#/resources/${orgid}`}>
+                Resources
+              </Nav.Link>
+              <Nav.Link className="mx-auto" href={`#/schedule/${orgid}`}>
+                Schedule
+              </Nav.Link>
+              <Nav.Link className="mx-auto" href={`#/chat/${orgid}`}>
+                Chat
+              </Nav.Link>
+              <Nav.Link
+                className="mx-auto"
+                href={`#/user/${orgid ? orgid : ""}`}
+              >
+                Profile
+              </Nav.Link>
             </Nav>
-          )}
-          {/* If a user is logged in, show these links */}
-          {user.id && (
-            <Nav className="ms-auto">
-              <Nav.Link href={`#/resources/${orgid}`}>Resources</Nav.Link>
-              <Nav.Link href={`#/schedule/${orgid}`}>Schedule</Nav.Link>
-              <Nav.Link href={`#/chat/${orgid}`}>Chat</Nav.Link>
-              <Nav.Link href={`#/user/${orgid ? orgid : ""}`}>Profile</Nav.Link>
-            </Nav>
-          )}
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>

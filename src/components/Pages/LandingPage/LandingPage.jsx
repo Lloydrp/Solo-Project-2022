a2af5006fb42905269bc7d056d08613d8d9619da
Lloyd Prock <lloyd.prock@gmail.com>
2022-10-26 14:52:09 -0500

    landing page done

--- src/components/Pages/LandingPage/LandingPage.jsx
+++ src/components/Pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+const image = require("../../../images/organization-station-background.png");
 
 function LandingPage() {
   const history = useHistory();
@@ -8,10 +9,27 @@ function LandingPage() {
     history.push("/registration");
   }
   return (
-    <main className="landing-page">
-      <h1>Organization Station</h1>
-      <button onClick={getStartHandler}>Get Started</button>
-      <a href="/#/login">Already a user? Login</a>
+    <main
+      className="bg-image d-flex flex-column align-items-center vh-100 justify-content-center"
+      style={{
+        backgroundImage: `url(${image})`,
+        height: `100vh`,
+        width: `100vw`,
+      }}
+    >
+      <div className="bg-light w-75 h-50 rounded-4 d-flex flex-column justify-content-center align-items-center">
+        <h1 className="text-primary mb-3 fw-bold">Organization Station</h1>
+        <button
+          type="button"
+          className="btn btn-primary mb-3"
+          onClick={getStartHandler}
+        >
+          Get Started
+        </button>
+        <a className="text-secondary" href="/#/login">
+          Already a user? Login
+        </a>
+      </div>
     </main>
   );
 }

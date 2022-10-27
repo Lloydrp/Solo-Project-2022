4b0d85d81ad7d4ca55693c66447d8065b7dcda69
Lloyd Prock <lloyd.prock@gmail.com>
2022-10-27 09:15:49 -0500

    organization edit documentation prior to refactor

--- src/components/UserOrganizationEdit/UserOrganizationEdit.jsx
+++ src/components/UserOrganizationEdit/UserOrganizationEdit.jsx
@@ -4,13 +4,16 @@ import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 
 function UserOrganizationEdit({ organization, setToggleEditOrganization }) {
+  // Setup redux variables
   const dispatch = useDispatch();
   const orgTypes = useSelector((store) => store.organization.orgTypes);
   const orgTitles = useSelector((store) => store.organization.orgTitles);
   const unsortedOrgUsers = useSelector((store) => store.organization.orgUsers);
+  // Sorting users by admin
   const orgUsers = unsortedOrgUsers.sort((userOne, userTwo) =>
     userOne.is_admin === userTwo.is_admin ? 0 : userOne.is_admin ? -1 : 1
   );
+  // Setup local state for inputs, toggles, and available booleans
   const [changeOrganizationName, setChangeOrganizationName] = useState("");
   const [addNewUser, setAddNewUser] = useState("");
   const [addAdminStatus, setAddAdminStatus] = useState("");
@@ -21,9 +24,11 @@ function UserOrganizationEdit({ organization, setToggleEditOrganization }) {
   const [toggleRemoveTitle, setToggleRemoveTitle] = useState(false);
   const [orgNameAvailable, setOrgNameAvailable] = useState(false);
 
+  // Begin function to handle name change submit
   function handleOrgNameSubmit(event) {
+    // Prevent form refresh
     event.preventDefault();
-
+    // Dispatch to saga to change organization name
     dispatch({
       type: "CHANGE_ORGANIZATION_NAME",
       payload: {
@@ -31,13 +36,15 @@ function UserOrganizationEdit({ organization, setToggleEditOrganization }) {
         organization_id: organization.organization_id,
       },
     });
-
+    // Clear organization state
     setChangeOrganizationName("");
-  }
+  } // End handleOrgNameSubmit
 
+  // Begin function to handle organization type change
   function handleOrgTypeSubmit(event) {
+    // Prevent form refresh
     event.preventDefault();
-
+    // Dispatch to saga to change organization type
     dispatch({
       type: "CHANGE_ORGANIZATION_TYPE",
       payload: {
@@ -45,47 +52,47 @@ function UserOrganizationEdit({ organization, setToggleEditOrganization }) {
         organization_id: organization.organization_id,
       },
     });
-  }
+  } // End handleOrgTypeSubmit
 
+  // Begin function to handle organization name input and reset availability to false
   function handleOrganizationNameInput(event) {
     setChangeOrganizationName(event.target.value);
     setOrgNameAvailable(false);
-  }
+  } // End handleOrganizationNameInput
 
   function handleOrgNameCancel(event) {
     event.preventDefault();
     setToggleOrganizationName(false);
-  }
+  } // End handleOrgNameCancel
 
   function handleOrgTypeCancel(event) {
     event.preventDefault();
     setToggleOrganizationType(false);
-  }
+  } // End handleOrgTypeCancel
 
   function handleAddUser(event) {
+    // Prevent form refresh
     event.preventDefault();
-
+    // Dispatch to saga to add new user
     dispatch({
       type: "ADD_TO_ORGANIZATION",
       payload: {
         newUser: addNewUser,
+        // If title is null send null otherwise send selected title
         title_id: addNewTitle.value === "null" ? null : addNewTitle.value,
         organization_id: organization.organization_id,
       },
     });
-
+    // Clear add user state
     setAddNewUser("");
-  }
+  } // End handleAddUser
 
   function handleAddAdmin(event) {
+    // Prevent form refresh
     event.preventDefault();
-
-    console.log({
-      newAdmin: addAdminStatus,
-      organization_id: organization.organization_id,
-    });
-
+    // Check if the Add or Remove admin button was clicked if add, add user to admin status
     if (event.nativeEvent.submitter.value === "add") {
+      // Dispatch to saga to add admin status
       dispatch({
         type: "ADD_ADMIN_STATUS",
         payload: {
@@ -93,8 +100,11 @@ function UserOrganizationEdit({ organization, setToggleEditOrganization }) {
           organization_id: organization.organization_id,
         },
       });
+      // Clear admin status state
       setAddAdminStatus("");
+      // If not add then remove the user from admin status
     } else if (event.nativeEvent.submitter.value === "remove") {
+      // Dispatch to saga to remove admin status
       dispatch({
         type: "REMOVE_ADMIN_STATUS",
         payload: {
@@ -102,13 +112,16 @@ function UserOrganizationEdit({ organization, setToggleEditOrganization }) {
           organization_id: organization.organization_id,
         },
       });
+      // Clear admin status state
       setAddAdminStatus("");
     } else {
       console.log("Error in add/remove admin submission");
     }
-  }
+  } // End handleAddAdmin
 
+  // Begin function to remove user from organization
   function handleRemoveFromOrg(id) {
+    // Dispatch to saga to remove user from organization
     dispatch({
       type: "REMOVE_FROM_ORG",
       payload: {
@@ -116,16 +129,17 @@ function UserOrganizationEdit({ organization, setToggleEditOrganization }) {
         organization_id: organization.organization_id,
       },
     });
-  }
+  } // End handleRemoveFromOrg
 
   function handleAddTitleCancel(event) {
     event.preventDefault();
     setToggleAddTitle(false);
-  }
+  } // End handleAddTitleCancel
 
   function handleAddTitle(event) {
+    // Prevent form refresh
     event.preventDefault();
-
+    // Dispatch to saga to add a new title to organization
     dispatch({
       type: "ADD_TITLE",
       payload: {
@@ -133,12 +147,14 @@ function UserOrganizationEdit({ organization, setToggleEditOrganization }) {
         newTitle: addTitle,
       },
     });
-
+    // Clear add title state and reset toggle to off
     setAddTitle("");
     setToggleAddTitle(false);
-  }
+  } // End handleAddTitle
 
+  // Begin function to remove a title from organization
   function handleRemoveTitle(id) {
+    // Dispatch to saga to remove title from organization
     dispatch({
       type: "REMOVE_TITLE",
       payload: {
@@ -146,14 +162,17 @@ function UserOrganizationEdit({ organization, setToggleEditOrganization }) {
         title_id: id,
       },
     });
-  }
+  } // End handleRemoveTitle
 
+  // Begin useEffect to check if organization name is in use
   useEffect(() => {
+    // If there is an organization name in the input, check if it's available after a 500 millisecond delay for typing
     if (changeOrganizationName) {
       const delayDebounceFn = setTimeout(() => {
         axios
           .get(`/api/organization/checkorgname/${changeOrganizationName}`)
           .then((result) => {
+            // If data is empty then organization name is available
             result.data.length === 0
               ? setOrgNameAvailable(true)
               : setOrgNameAvailable(false);
@@ -165,7 +184,7 @@ function UserOrganizationEdit({ organization, setToggleEditOrganization }) {
 
       return () => clearTimeout(delayDebounceFn);
     }
-  }, [changeOrganizationName]);
+  }, [changeOrganizationName]); // End useEffect for organization availability
 
   return (
     <>
